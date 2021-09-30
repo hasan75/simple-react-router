7280cd2cae4955e2a29fe7f485d976e3c173d715
Hasan75 <hasanjab14@gmail.com>
2021-09-30 19:46:55 +0600

    route added differenty

--- src/App.js
+++ src/App.js
@@ -4,14 +4,24 @@ import Home from './components/Home/Home';
 import Friends from './components/Friends/Friends';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
-      <NotFound></NotFound>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/friends">
+            <Friends></Friends>
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
