d6cb6cbeeb75e220133906cb3dc427de01734c17
Hasan75 <hasanjab14@gmail.com>
2021-09-30 23:48:29 +0600

    react-simple grid added

--- src/App.js
+++ src/App.js
@@ -4,30 +4,11 @@ import Home from './components/Home/Home';
 import Friends from './components/Friends/Friends';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route path="/home">
-            <Home></Home>
-          </Route>
-          <Route path="/about">
-            <About></About>
-          </Route>
-          <Route path="/friends">
-            <Friends></Friends>
-          </Route>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route>
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Friends></Friends>
     </div>
   );
 }
--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
+import './Friends.css'
 
 const Friends = () => {
+    const [friends, setFriends] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setFriends(data))
+    })
     return (
         <div>
             <h2>This is Friends</h2>
+            <h1>All The friends: {friends.length}</h1>
+            <div className="friendContainer">
+            {
+                friends.map(friend => <Friend
+                    key={friend.id}
+                    friend={friend}
+                ></Friend>)
+            }
+            </div>
         </div>
     );
 };
