a841e46c6699d2b3ea8c4e46af2e915534500b50
Hasan75 <hasanjab14@gmail.com>
2021-10-01 01:25:11 +0600

    link added for react

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import Home from './components/Home/Home';
 import Friends from './components/Friends/Friends';
 import About from './components/About/About';
 import NotFound from './components/NotFound/NotFound'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+
       <Router>
+      <Link to="/home">Home</Link>
+      <Link to="/friends">Friends</Link>
+      <Link to="/about">About</Link>
         <Switch>
         <Route exact path='/'>
             <Home>
