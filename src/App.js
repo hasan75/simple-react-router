bbaaee79fdbd19d976547e0d0c597ff2502d32c0
Hasan75 <hasanjab14@gmail.com>
2021-09-30 18:38:01 +0600

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
+import Friends from './components/Friends/Friends';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound'
 
 function App() {
   return (
     <div className="App">
-      
+      <Home></Home>
+      <Friends></Friends>
+      <About></About>
+      <NotFound></NotFound>
     </div>
   );
 }
