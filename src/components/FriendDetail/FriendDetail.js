7082c25f66b7a5aeafd835b86bbe1b3b64a33039
Hasan75 <hasanjab14@gmail.com>
2021-10-01 20:58:24 +0600

    commited with another useHistory() hook on friendsDetails page

--- src/components/FriendDetail/FriendDetail.js
+++ src/components/FriendDetail/FriendDetail.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 
 const FriendDetail = () => {
     const {friendId} = useParams();
     const [friend, setFriend] = useState({});
+    const history = useHistory();
+
     useEffect(()=>{
         const url = `https://jsonplaceholder.typicode.com/users/${friendId}`;
         fetch(url)
         .then(res => res.json())
         .then(data => setFriend(data))
-    },[])
+    },[]);
+
+    const handleClick = () => {
+        history.push('/friends')
+    }
     return (
         <div>
             <h2>Friend Detail of ID {friendId}</h2>
@@ -17,6 +23,7 @@ const FriendDetail = () => {
             <h2>{friend.phone}</h2>
             <h4>{friend.website}</h4>
             <p>Works at {friend.company?.name}</p>
+            <button onClick={handleClick}> See All Friends</button>
         </div>
     );
 };
